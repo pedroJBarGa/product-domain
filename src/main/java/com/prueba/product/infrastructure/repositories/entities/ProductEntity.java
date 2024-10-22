37e843feddac26010b54fcf69b3b1af5d2cb15c9
pedro-barrios <pedrojose.barrios.uhu@gmail.com>
2024-10-22 17:10:59 +0200

    add lombok annotations in entity and change in JPA annotation

--- src/main/java/com/prueba/product/infrastructure/repositories/entities/ProductEntity.java
+++ src/main/java/com/prueba/product/infrastructure/repositories/entities/ProductEntity.java
@@ -3,18 +3,15 @@ package com.prueba.product.infrastructure.repositories.entities;
 import java.math.BigDecimal;
 import java.sql.Timestamp;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "product")
 public class ProductEntity {
 
@@ -42,24 +39,8 @@ public class ProductEntity {
   @Column(name = "curr")
   private String curr;
 
-  @ManyToOne(optional = false, cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+  @ManyToOne(optional = false, cascade = CascadeType.ALL, fetch = FetchType.LAZY)
   @JoinColumn(name = "brand_id")
   private GroupEntity group;
 
-  public ProductEntity() {
-  }
-
-  public ProductEntity(Integer id, long productId, Timestamp startDate,
-                       Timestamp endDate, int priceList, int priority,
-                       BigDecimal price, String curr, GroupEntity group) {
-    this.id = id;
-    this.productId = productId;
-    this.startDate = startDate;
-    this.endDate = endDate;
-    this.priceList = priceList;
-    this.priority = priority;
-    this.price = price;
-    this.curr = curr;
-    this.group = group;
-  }
 }
